adedbe9e9f7934fd40595bf4ed29ad06844dcc10
amirganmor <amirganmor@gmail.com>
2020-03-28 16:41:49 +0300

    Update CondBoth.cpp

--- threads/CondBoth.cpp
+++ threads/CondBoth.cpp
@@ -8,32 +8,23 @@
 #include <iostream>
 #include <string>
 #include <time.h>
-
 using namespace std;
 
 mutex mu;
 condition_variable cv;
-int ctr = 0;
-
-
+bool ready = true;
 
 void func() {
 	for (int i = 0; i <= 10; i++) {
 		static int count = 0;
-		count++;
 		{
 			std::unique_lock<std::mutex> guard(mu);
+			count++;
 			cout << "thread : " << count << "     :t1" << endl;
-			ctr++;
-			if (ctr == 2) {
-				ctr = 0;
-				cv.notify_all();
-			}
-			cv.wait(guard, [] {return !ctr; });
-
 		}
-		std::this_thread::sleep_for(std::chrono::milliseconds(10));
-
+		std::this_thread::sleep_for(std::chrono::milliseconds(5000));
+		ready = true;
+		cv.notify_all();
 
 	}
 }
@@ -41,18 +32,14 @@ void func() {
 void func2() {
 	for (int i = 0; i <= 10; i++) {
 		static int count2 = 0;
-		count2++;
 		{
 			std::unique_lock<std::mutex> guard(mu);
+			count2++;
 			cout << "thread : " << count2 << "     :t2 " << endl;
-			ctr++;
-			if (ctr == 2) {
-				ctr = 0;
-				cv.notify_all();
-			}
-			cv.wait(guard, [] {return !ctr; });
+			cv.wait(guard, [] {return ready; });
+			ready = false;
 		}
-		std::this_thread::sleep_for(std::chrono::milliseconds(10));
+		std::this_thread::sleep_for(std::chrono::milliseconds(500));
 
 	}
 }
@@ -66,8 +53,10 @@ int main()
 	std::thread t2(func2);
 	t1.join();
 	t2.join();
+
 	printf("Time taken: %.2fs\n", (double)(clock() - tStart) / CLOCKS_PER_SEC);
 	cout << endl << "finished! press key to end" << endl;
+
 	cin.get();
 	return 0;
 }
